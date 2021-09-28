0497368dc31525afcc2347130633241c813b18b8
Swabin <88606840+Swabin@users.noreply.github.com>
2021-09-28 14:28:14 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,8 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} <br> ⚜️Group⚜️ @SBcinemalokam
+🔰Channel🔰 @SBcinemakal,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
